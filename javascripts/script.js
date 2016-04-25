9c207e136f7381fe0f670c50302c7d43cf6a8003
iphrodo <iphrodo@gmail.com>
2016-04-25 21:02:28 +0300

    Added files via upload

---
+++ javascripts/script.js
@@ -0,0 +1,12 @@
+var time1 =1200,
+	time2 = 1500,
+	time = Math.random(time1,time2),
+	width  = window.innerWidth,
+	nool = 0,
+	i = 0
+	height  = window.innerHeight;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
